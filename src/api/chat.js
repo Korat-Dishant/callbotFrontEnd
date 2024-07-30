a26e90190d4ef06ab746f1163f566b61f2023670
Korat Dishant <koratdishant536631@gmail.com>
2024-07-30 22:54:20 +0530

    fix

--- src/api/chat.js
+++ src/api/chat.js
@@ -2,7 +2,7 @@ import { get_prods } from "../../utils";
 
 const uri = import.meta.env.VITE_APP_SERVER_URI;
 export const ask = async (query,user_name,user_email,date) => {
-  // console.log("calling = " , `${uri}/ask?user_query=${query}`);
+  console.log("calling = " , `${uri}/ask?user_query=${query}`);
 
   const response = await fetch(
     `${uri}/ask?user_query=${query}&user_name=${user_name}&user_email=${user_email}&date=${date}`,
@@ -12,7 +12,7 @@ export const ask = async (query,user_name,user_email,date) => {
   );
   const data = await response.json();
   const products = get_prods(data.response);
-  // console.log("received data = " , data);
+  console.log("received data = " , data);
   return {
     success: true,
     data: { message: data.response, products: products },
