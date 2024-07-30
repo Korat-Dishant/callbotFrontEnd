4fb961679045afea09f67b812a5402eecc710441
Korat Dishant <koratdishant536631@gmail.com>
2024-07-30 21:22:34 +0530

    update api

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,19 +1,20 @@
 import { get_prods } from "../../utils";
 
 const uri = import.meta.env.VITE_APP_SERVER_URI;
-export const ask = async (query) => {
-    // console.log("calling = " , `${uri}/ask?user_query=${query}`);
+export const ask = async (query,user_name,user_email,date) => {
+  // console.log("calling = " , `${uri}/ask?user_query=${query}`);
 
-    // console.log("user =======================" , user);
-    // console.log("user name" , user.user.fullName);
-    // console.log("email" , user.user.primaryEmailAddress.emailAddress);
-    // console.log("date" , user.user.lastSignInAt.getDate());
-    // console.log("month" , user.user.lastSignInAt.getMonth()+1);
-    const response = await fetch(`${uri}/ask?user_query=${query}`, {
-        method: "POST"
-    });
-    const data = await response.json();
-    const products = get_prods(data.response);
-    // console.log("received data = " , data);
-    return {success: true, data: {message: data.response, products: products}};
-}
+  const response = await fetch(
+    `${uri}/ask?user_query=${query}&user_name=${user_name}&user_email=${user_email}&date=${date}`,
+    {
+      method: "POST",
+    }
+  );
+  const data = await response.json();
+  const products = get_prods(data.response);
+  // console.log("received data = " , data);
+  return {
+    success: true,
+    data: { message: data.response, products: products },
+  };
+};
