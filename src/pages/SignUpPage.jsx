587a8c28b6741396a2b1fb8bb5bc691897c85c77
Korat Dishant <koratdishant536631@gmail.com>
2024-07-30 22:49:10 +0530

    clerk forceRedirectUrl

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,11 +1,12 @@
-import { SignUp } from '@clerk/clerk-react'
+import { SignUp } from "@clerk/clerk-react";
+const uri = import.meta.env.VITE_APP_SERVER_URI;
 
 const SignUpPage = () => {
-    return (
-        <div className='w-full flex justify-center'>
-            <SignUp forceRedirectUrl="http://localhost:5173/chat"/>
-        </div>
-    )
-}
+  return (
+    <div className="w-full flex justify-center">
+      <SignUp forceRedirectUrl={`${uri}/chat`} />
+    </div>
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
